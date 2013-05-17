a9bab32ec3acf85103ecc4701da13730b80790ae
Luka Mirosevic <luka@goonbee.com>
2013-05-17 12:33:52 +0200

    Initial Commit

---
+++ GBRetractableTabBar/GBRetractableTabBar.h
@@ -0,0 +1,13 @@
+//
+//  GBRetractableTabBar.h
+//  GBRetractableTabBar
+//
+//  Created by Luka Mirosevic on 17/05/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBRetractableTabBar : NSObject
+
+@end
---
+++ GBRetractableTabBar/GBRetractableTabBar.m
@@ -0,0 +1,13 @@
+//
+//  GBRetractableTabBar.m
+//  GBRetractableTabBar
+//
+//  Created by Luka Mirosevic on 17/05/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBRetractableTabBar.h"
+
+@implementation GBRetractableTabBar
+
+@end
